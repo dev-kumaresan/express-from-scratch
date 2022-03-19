968dc2ea93039e2c0142841d054dedc6913fe75b
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-03-19 19:14:21 +0530

    cookies_creation

--- Express/Basic_routing/index.js
+++ Express/Basic_routing/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const controller = require('./controller/index.controller')
+
+
 app.get('/hi',(req,res)=>{
     res.send("Hi kumaresan");
 })
-app.get('/about', (req, res) => {
+
+
+app.get('/about', (req, res) => {''
     res.send("Hi kumaresan I am from Alangudi");
 })
+
+
+
+// abs
+
+app.get("/show-data",controller.showUserData)
+
+
+
+
 //LocalHost creation
-app.listen(2001,()=>{
+const port = process.env.PORT || 2001;
+app.listen(port,()=>{
     console.log("Server started on 2001");
 });
--- Express/Cookies/index.js
+++ Express/Cookies/index.js
@@ -1,1 +1,8 @@
-
+const express = require('express');
+const app = express();
+const cookie_parser = require('cookie-parser');
+app.use(cookie_parser());
+app.get('/',function(req,res){
+res.cookie('name','express').send('cookie set');
+});
+app.listen(3000);
