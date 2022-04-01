d975d1005c6cca71ca6b4c1c07e03307b84a12f9
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-01 18:15:37 +0530

    cookies

--- Express/Cookies/cookies-Expiration/index.js
+++ Express/Cookies/cookies-Expiration/index.js
@@ -3,7 +3,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 app.get('/',(req,res)=>{
-res.cookie('name', 'value', {expires: new Date(new Date().getTime()+60*1000)}).send('cooie-expiration-set'); 
+res.cookie('name', 'value', {expires: new Date(new Date().getTime()+60*1000)}).send('cookie-expiration-set'); 
 });
 app.listen(8000,()=>{
     console.log('server started on 8000');
