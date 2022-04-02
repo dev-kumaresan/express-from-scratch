3705413d1be024b4d9767c9bfb9c2b4a0fbb948c
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-02 20:53:32 +0530

    GET-Method

--- Express/RESTFul_APIs/Methods/GET/index.js
+++ Express/RESTFul_APIs/Methods/GET/index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+app.get('/hi',(req,res)=>{
+    res.send("Hi kumaresan");
+})
+app.listen(PORT,()=>{
+    console.log("Server started at port 4000");
+})
 
